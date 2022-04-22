396e1f3429a757d81402e03f4026bf175148d291
Ruben Campos <ruben@gmail.com>
2022-04-22 21:54:35 +0100

    make it better 2.0

--- models/database.js
+++ models/database.js
@@ -1,14 +1,27 @@
 var Sequelize = require('sequelize')
 
 const sequelize = new Sequelize(
-  'saferoom_db', //bd name
+  /*'saferoom_db', //bd name
   'postgres',      //user
   'ruben123',   //password
   { 
     host: 'localhost',
     port: '5432',
     dialect: 'postgres'
-  }
+  }*/
+  'd9rufjoo05vjna', //db
+  'zoxetsctlkuizb', //user
+  '184c9c7c3382a5a29c58e681e4eae429932cd4be4243ec403955ed44dd6404b6', //password
+  {
+    host: 'ec2-63-32-248-14.eu-west-1.compute.amazonaws.com',
+    port: '5432',
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl:{
+            rejectUnauthorized: false
+        }
+    }
+}
 )
 
 module.exports = sequelize
