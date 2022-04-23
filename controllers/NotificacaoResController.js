a0e8abfa1df2e7bc3d226b17d204fb1c828fa88b
Ruben Campos <ruben@gmail.com>
2022-04-23 16:27:46 +0100

    SafeRoom_Backend_23/04

--- controllers/NotificacaoResController.js
+++ controllers/NotificacaoResController.js
@@ -1,5 +1,7 @@
 var sequelize = require('../models/database')
 var NotificacaoReserva = require('../models/notificacaoReserva')
+var Reservas = require('../models/reserva')
+var Notificacoes = require('../models/notificacao')
 
 const controllers_NotificacaoReserva = {}
 
@@ -7,14 +9,20 @@ sequelize.sync()
 
 //-------------List-------------
 controllers_NotificacaoReserva.listarNotificacaoReserva = async(req, res) => {
-  const data = await NotificacaoReserva.findAll()
+  const data = await sequelize.query('SELECT notificacoes.mensagem, reservas.id_reserva ' +
+                                      'FROM notificacoes JOIN notificacoes_reservas ' +
+                                      'ON notificacoes."id_notificacao" = notificacoes_reservas."id_notificacao" JOIN reservas ' +
+                                      'ON notificacoes_reservas."id_reserva" = reservas."id_reserva" ',{
+      model: Notificacoes, NotificacaoReserva, Reservas, 
+      mapToModel: true
+  })
   .then(function(data){
-    return data
+      return data
   })
-  .catch(err => {
-    return err
+  .catch(err =>{
+      return err
   })
-  res.json({Success: true, data, data})
+  res.json({success: true, data: data})
 }
 
 //-------------View by ID-------------
--- controllers/UserCentroController.js
+++ controllers/UserCentroController.js
@@ -1,5 +1,7 @@
 var sequelize = require('../models/database')
 var UtilizadorCentro = require('../models/utilizadorCentro')
+var Utilizadores = require('../models/utilizador')
+var Centros = require('../models/centro')
 
 const controllers_UtilizadorCentro = {}
 
@@ -7,14 +9,20 @@ sequelize.sync()
 
 //-------------List-------------
 controllers_UtilizadorCentro.listarUtilizadorCentro = async(req, res) => {
-  const data = await UtilizadorCentro.findAll()
+  const data = await sequelize.query('SELECT utilizadores.nome AS nome_user, centros.nome AS nome_centro ' +
+                                      'FROM utilizadores JOIN utilizadores_centros ' +
+                                      'ON utilizadores."id_user" = utilizadores_centros."id_user" JOIN centros ' +
+                                      'ON utilizadores_centros."id_centro" = centros."id_centro" ',{
+      model: Utilizadores, UtilizadorCentro, Centros, 
+      mapToModel: true
+  })
   .then(function(data){
-    return data
+      return data
   })
-  .catch(err => {
-    return err
+  .catch(err =>{
+      return err
   })
-  res.json({Success: true, data, data})
+  res.json({success: true, data: data})
 }
 
 //-------------View by ID-------------
